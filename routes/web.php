6f47684e99b2a73993e237962e523be5e85d76fd
atik <atikercan35@gmail.com>
2019-07-29 15:17:05 +0000

    fixed route for showing company

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,14 @@ Route::get('/', function () {
 });
 
 Route::get('companies', 'CompaniesController@index');
-// Other company routes?
+Route::get('companies/{id}', 'CompaniesController@show');
+
+
+Route::get('companies/{id}/vue', 'CompaniesController@showVue');
+
+Route::post('companies', 'CompaniesController@store');
+Route::put('companies/{id}', 'CompaniesController@update');
+
 Route::delete('companies/{id}', 'CompaniesController@destroy');
 
 Route::get('companies_vue/{id}', function ($id) {
